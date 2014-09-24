e14ecfe600d2b65a42af66f275599a781a7918e6
rob-alarcon <rob.alarcon@gmail.com>
2014-09-24 16:24:01 -0700

    Model for Users

---
+++ js/model/User.js
@@ -0,0 +1,24 @@
+/**
+* This is the model for a Player
+*
+**/
+var Card = function(name) {
+	
+	/**
+	* Holds the nickname of the user
+	**/	
+	this.name = name;
+
+	/**
+	* Holds an array of cards for the user
+	**/
+	this.cards = [];
+};
+
+/**
+* This is the model for a Player
+*
+**/
+Card.prototype.fillUserDeck = function() {
+
+};
